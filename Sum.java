2c45da892472eeec284bae7da892ab82b76b2bf5
ethanhopkins <112411800+ethanhopkins@users.noreply.github.com>
2022-08-29 11:51:58 -0400

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum
+{
+public static void main(String[] args)
+{
+	int number1 = 3;
+	int number2 = 5;
+	int sum = number1 + number2;
+	int mult = number1 * number2;
+	int sub = number1 - number2;
+	System.out.println(sum);
+	System.out.println(mult);
+	System.out.println(sub);
+}
+}
